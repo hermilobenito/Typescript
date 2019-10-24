5616303e1b8fa0ebcaf1624b57038a3878c2f319
hermilobenito <hermilobenito@gmail.com>
2019-10-24 10:54:35 -0500

    Hello world in typescript

---
+++ greeter.ts
@@ -0,0 +1,5 @@
+function greeter(person){
+  return "Hello, "+person;
+}
+let user ="Hermilo User";
+document.body.textContent=greeter(user);
--- index.ts
+++ index.ts
@@ -2,5 +2,13 @@
 import './style.css';
 
 // Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+//const appDiv: HTMLElement = document.getElementById('app');
+//appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+
+function greeter(person){
+  return "Hello, "+person;
+}
+
+let user ="Hermilo User";
+console.log(user);
+document.body.textContent=greeter(user);
